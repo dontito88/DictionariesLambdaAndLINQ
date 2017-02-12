604bec53c4b598f477671b47ce29522dabb3427f
TrifonEstov <don.tito.g@gmail.com>
2017-02-13 00:18:50 +0200

    Regular commit

---
+++ DictionariesLambdaAndLINQ/LastThreeNumbers/LastThreeNumbers.cs
@@ -0,0 +1,24 @@
+﻿namespace LastThreeNumbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class LastThreeNumbers
+    {
+        public static void Main()
+        {
+            string[] input = Console.ReadLine()
+                .Split(' ');
+
+            List<int> nums = input
+                .Select(int.Parse)
+                .ToList();
+
+            var sortedNumbers = nums.OrderByDescending(x => x);
+            var largest3Numbers = sortedNumbers.Take(3);
+
+            Console.WriteLine(string.Join(" ", largest3Numbers));
+        }
+    }
+}
