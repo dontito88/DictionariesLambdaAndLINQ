bad1f361c95ee384caa70f7ce740649a342c1769
TrifonEstov <don.tito.g@gmail.com>
2017-02-13 00:08:47 +0200

    New program added!

---
+++ DictionariesLambdaAndLINQ/SumMinMaxAverage/SumMinMaxAverage.cs
@@ -0,0 +1,24 @@
+﻿namespace SumMinMaxAverage
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class SumMinMaxAverage
+    {
+        public static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[] nums = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                nums[i] = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Sum = {0}", nums.Sum());
+            Console.WriteLine("Min = {0}", nums.Min());
+            Console.WriteLine("Max = {0}", nums.Max());
+            Console.WriteLine("Average = {0}", nums.Average());
+        }
+    }
+}
