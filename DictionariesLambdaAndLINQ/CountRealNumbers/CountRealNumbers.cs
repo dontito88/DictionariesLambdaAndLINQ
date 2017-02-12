17bceb3c7a55bbe10f29e08f77d217bb3b5d4bb2
TrifonEstov <don.tito.g@gmail.com>
2017-02-12 16:57:28 +0200

    another problem solved!

---
+++ DictionariesLambdaAndLINQ/CountRealNumbers/CountRealNumbers.cs
@@ -0,0 +1,29 @@
+﻿namespace CountRealNumbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class CountRealNumbers
+    {
+        public static void Main()
+        {
+            var numbers = Console.ReadLine().Split().Select(double.Parse);
+
+            var result = new SortedDictionary<double, int>();
+
+            foreach (var number in numbers)
+            {
+                if (!result.ContainsKey(number))
+                {
+                    result[number] = 0;
+                }
+                result[number]++;
+            }
+            foreach (var item in result)
+            {
+                Console.WriteLine($"{item.Key} -> {item.Value}");
+            }
+        }
+    }
+}
